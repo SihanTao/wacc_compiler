b258b38aec8b881c2db6c526b954823673d242dc
Lin,Yijun <yl6220@ic.ac.uk>
2022-02-01 10:48:06 -0800

    implement Parser.sh for parser test

---
+++ src/test/compile/Parser.sh
@@ -0,0 +1,60 @@
+VALID_TEST=$(find ./src/test/valid -iname '*.wacc')
+INVALID_TEST=$(find ./src/test/invalid -iname '*.wacc')
+
+valid_cases=0
+invalid_cases=0
+total_cases=0
+valid_cases_for_invalid=0
+invalid_cases_for_invalid=0
+total_cases_for_invalid=0
+
+echo "-------------------------------------------------------------------"
+echo " VALID TEST START"
+echo "-------------------------------------------------------------------"
+
+for file in ${VALID_TEST}; do
+    msg=$(./compile $file 2>> ./bin/valid)
+    total_cases=$((total_cases+1))
+
+    if [ $? -ne 0 ]; then
+      echo "-------------------------------------------------------"
+      echo "fail at" $file
+      echo "error message: " $msg
+      echo "-------------------------------------------------------"
+      exit 1
+      invalid_cases=$((invalid_cases+1))
+    else
+      valid_cases=$((valid_cases+1))
+    fi
+    done
+
+    echo "-------------------------------------------------------"
+    echo "TOTAL" $total_cases "CASES. PASSED" $valid_cases ". FAILED" $invalid_cases "CASES"
+
+echo "-------------------------------------------------------------------"
+echo " INVALID TEST START"
+echo "-------------------------------------------------------------------"
+
+
+for file in ${INVALID_TEST}; do
+    msg=$(./compile $file 2>> ./bin/invalid)
+    total_cases=$((total_cases_for_invalid+1))
+
+    if [ $? -ne 0 ]; then
+      echo "-------------------------------------------------------"
+      echo "fail at" $file
+      echo "error message: " $msg
+      echo "-------------------------------------------------------"
+      exit 1
+      invalid_cases=$((invalid_cases_for_invalid+1))
+    else
+      valid_cases=$((valid_cases_for_invalid+1))
+    fi
+    done
+
+    echo "-------------------------------------------------------"
+    echo "TOTAL" $total_cases "CASES. PASSED" $valid_cases ". FAILED" $invalid_cases "CASES"
+
+echo "-------------------------------------------------------------------"
+echo " PARSER TEST FINISHED"
+echo "-------------------------------------------------------------------"
