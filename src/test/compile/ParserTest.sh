4048bdedd6aaa456fb7aafd0386e1c072cbefd63
Lin,Yijun <yl6220@ic.ac.uk>
2022-02-01 15:04:39 -0800

    fixed and renamed ParserTest.sh to make it able to run test

--- src/test/compile/ParserTest.sh
+++ src/test/compile/ParserTest.sh
@@ -1,5 +1,5 @@
-VALID_TEST=$(find ./src/test/valid -iname '*.wacc')
-INVALID_TEST=$(find ./src/test/invalid -iname '*.wacc')
+VALID_TEST=$(find ../valid -iname '*.wacc')
+INVALID_TEST=$(find ../invalid -iname '*.wacc')
 
 valid_cases=0
 invalid_cases=0
@@ -13,8 +13,8 @@ echo " VALID TEST START"
 echo "-------------------------------------------------------------------"
 
 for file in ${VALID_TEST}; do
-    msg=$(./compile $file 2>> ./bin/valid)
-    total_cases=$((total_cases+1))
+    msg=$(./compile $file 2>> ../bin/valid)
+    let total_cases+=1
 
     if [ $? -ne 0 ]; then
       echo "-------------------------------------------------------"
@@ -22,14 +22,15 @@ for file in ${VALID_TEST}; do
       echo "error message: " $msg
       echo "-------------------------------------------------------"
       exit 1
-      invalid_cases=$((invalid_cases+1))
+      let invalid_cases+=1
     else
-      valid_cases=$((valid_cases+1))
+      let valid_cases+=1
     fi
     done
 
     echo "-------------------------------------------------------"
     echo "TOTAL" $total_cases "CASES. PASSED" $valid_cases ". FAILED" $invalid_cases "CASES"
+    echo "-------------------------------------------------------"
 
 echo "-------------------------------------------------------------------"
 echo " INVALID TEST START"
@@ -37,7 +38,7 @@ echo "-------------------------------------------------------------------"
 
 
 for file in ${INVALID_TEST}; do
-    msg=$(./compile $file 2>> ./bin/invalid)
+    msg=$(./compile $file 2>> ../bin/invalid)
     total_cases=$((total_cases_for_invalid+1))
 
     if [ $? -ne 0 ]; then
