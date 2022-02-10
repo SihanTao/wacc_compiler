4442c14fd13ce0d942a0b571f8761238f4622827
Adam Jones <ayj20@ic.ac.uk>
2022-02-10 11:21:08 -0800

    attempted unreachable code detection

--- src/WACCSemanticAnalyser/ErrorRecorder.java
+++ src/WACCSemanticAnalyser/ErrorRecorder.java
@@ -1,11 +1,26 @@
 package WACCSemanticAnalyser;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+
 public class ErrorRecorder {
 
-	private int errorCount = 0;
+	private int errorCount;
+
+	public ErrorRecorder() {
+		this.errorCount = 0;
+	}
 
 	public int getErrors() {
 		return errorCount;
+	}
+
+	public void addUnreachableCodeError(ParserRuleContext ctx) {
+		addError(ctx, "unreachable code detected");
+	}
+
+	private void addError(ParserRuleContext ctx, String msg) {
+		System.out.println("Semantic error detected at " + ctx.getStart().getLine() + ":" + ctx.getStart().getCharPositionInLine() + " msg: " + msg);
+		errorCount = errorCount + 1;
 	}
 
 }
--- src/WACCSemanticAnalyser/WACCSemanticErrorVisitor.java
+++ src/WACCSemanticAnalyser/WACCSemanticErrorVisitor.java
@@ -11,4 +11,23 @@ public class WACCSemanticErrorVisitor<T> extends WACCParserBaseVisitor<T> {
 		this.errorRecorder = errorRecorder;
 	}
 
+	@Override public T visitStat(WACCParser.StatContext ctx) {
+		if (ctx.IF() == null && ctx.stat(1) != null && isClosedStatement(ctx.stat(0))) {
+			errorRecorder.addUnreachableCodeError(ctx.stat(1));
+		}
+		return visitChildren(ctx);
+	}
+
+	// helper function to check if a statement is "closed" i.e. does not
+	// allow logic to proceed
+	private boolean isClosedStatement(WACCParser.StatContext ctx) {
+		if (ctx.RETURN() != null) {
+			return true;
+		} else if (ctx.IF() != null) {
+			return isClosedStatement(ctx.stat(0)) && isClosedStatement(ctx.stat(1));
+		} else {
+		return false;
+		}
+	}
+
 }
