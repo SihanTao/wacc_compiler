f6dc48e06ec7426c166ff93c94fb3283f5886471
Adam Jones <ayj20@ic.ac.uk>
2022-02-10 03:02:33 -0800

    fixed testing script, all invalid syntax fail properly, 17 of valid syntax fail

--- Test.sh
+++ Test.sh
@@ -4,6 +4,9 @@ VALID_TEST=$(find "${SRC_DIR}/valid" -iname '*.wacc')
 INVALID_SYNTAX_TEST=$(find "${SRC_DIR}/invalid/syntaxErr" -iname '*.wacc')
 INVALID_SEMANTIC_TEST=$(find "${SRC_DIR}/invalid/semanticErr" -iname '*.wacc')
 
+SYNTAX_ERROR_CODE=100
+SEMANTIC_ERROR_CODE=200
+
 valid_cases=0
 invalid_cases=0
 total_cases=0
@@ -18,17 +21,22 @@ echo "VALID TEST START"
 echo "-------------------------------------------------------------------"
 
 for file in ${VALID_TEST}; do
-    echo $file >> "${SRC_DIR}/out/valid.txt" 
-    ./compile $file >> "${SRC_DIR}/out/valid.txt"
+
     let total_cases+=1
 
-    if [ $? -ne 0 ]; then
+    echo $file >> "${SRC_DIR}/out/valid.txt" 
+    ./compile $file &>> "${SRC_DIR}/out/valid.txt"
+
+	let error_code=$?
+
+    if [ $error_code -ne 0 ]; then
       echo "-------------------------------------------------------"
       echo "fail at" $file
-      echo "error message: " $?
+      echo "error code: " $error_code
       echo "-------------------------------------------------------"
       let invalid_cases+=1
     else
+      echo $file " is valid"
       let valid_cases+=1
     fi
     done
@@ -46,17 +54,22 @@ invalid_cases_for_invalid=0
 total_cases_for_invalid=0
 
 for file in ${INVALID_SYNTAX_TEST}; do
-    echo $file >>  "${SRC_DIR}/out/invalidSyntax.txt"
-    ./compile $file >> "${SRC_DIR}/out/invalidSyntax.txt"
+
     let total_cases_for_invalid+=1
 
-    if [ $? -ne 100 ]; then
+    echo $file >>  "${SRC_DIR}/out/invalidSyntax.txt"
+    ./compile $file &>> "${SRC_DIR}/out/invalidSyntax.txt"
+
+	let error_code=$?
+
+    if [ $error_code -ne $SYNTAX_ERROR_CODE ]; then
       echo "-------------------------------------------------------"
       echo "fail at" $file
-      echo "error message: " $?
+      echo "error code: " $error_code
       echo "-------------------------------------------------------"
       let invalid_cases_for_invalid+=1
     else
+	  echo $file " fails correctly with error code " $SYNTAX_ERROR_CODE
       let valid_cases_for_invalid+=1
     fi
     done
@@ -75,17 +88,22 @@ invalid_cases_for_invalid=0
 total_cases_for_invalid=0
 
 for file in ${INVALID_SEMANTIC_TEST}; do
-    echo $file >> "${SRC_DIR}/out/invalidSemantic.txt"
-    ./compile $file >> "${SRC_DIR}/out/invalidSemantic.txt"
+
     let total_cases_for_invalid+=1
 
-    if [ $? -ne 100 ]; then
+    echo $file >> "${SRC_DIR}/out/invalidSemantic.txt"
+    ./compile $file &>> "${SRC_DIR}/out/invalidSemantic.txt"
+
+	let error_code=$?
+
+    if [ $error_code -ne $SEMANTIC_ERROR_CODE ]; then
       echo "-------------------------------------------------------"
       echo "fail at" $file
-      echo "error message: " $?
+      echo "error code: " $error_code
       echo "-------------------------------------------------------"
       let invalid_cases_for_invalid+=1
     else
+	  echo $file " fails correctly with error code " $SEMANTIC_ERROR_CODE
       let valid_cases_for_invalid+=1
     fi
     done
