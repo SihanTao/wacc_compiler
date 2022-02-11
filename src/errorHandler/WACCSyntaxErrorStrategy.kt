c256f32e302b73efb59188790b69318655e54113
Adam Jones <ayj20@ic.ac.uk>
2022-02-11 09:52:37 -0800

    extended error messaging

--- src/errorHandler/WACCSyntaxErrorStrategy.kt
+++ src/errorHandler/WACCSyntaxErrorStrategy.kt
@@ -1,11 +1,48 @@
 package errorHandler
 
 import org.antlr.v4.runtime.DefaultErrorStrategy
-import org.antlr.v4.runtime.Parser
-import org.antlr.v4.runtime.RecognitionException
+import org.antlr.v4.runtime.*
+import org.antlr.v4.runtime.misc.*
 
 class WACCSyntaxErrorStrategy : DefaultErrorStrategy() {
-    override fun reportError(parser: Parser, e: RecognitionException) {
-        parser.notifyErrorListeners(e.offendingToken, e.message, e)
+    fun reportError(parser: Parser, e: RecognitionException) {
+        when (e) {
+            is NoViableAltException -> {
+                reportNoViableAlternative(parser,
+                        e as NoViableAltException)
+            }
+            is FailedPredicateException -> {
+                reportFailedPredicate(parser,
+                        e as FailedPredicateException)
+            }
+            else -> {
+                parser.notifyErrorListeners(e.getOffendingToken(),
+                        e.getMessage(), e)
+            }
+        }
     }
+
+    private fun reportNoViableAlternative(parser: Parser,
+                                          e: NoViableAltException) {
+        val tokens: TokenStream = parser.getInputStream()
+        val input: String = if (tokens != null) {
+            if (e.getStartToken().getType() === -1) {
+                "<EOF>"
+            } else {
+                tokens.getText(e.getStartToken(), e.getOffendingToken())
+            }
+        } else {
+            "<unknown input>"
+        }
+        val msg = "No viable alternative at input $input"
+        parser.notifyErrorListeners(e.getOffendingToken(), msg, e)
+    }
+
+    private fun reportFailedPredicate(parser: Parser,
+                                      e: FailedPredicateException) {
+        val ruleName: String = parser.getRuleNames().get(parser.getContext().getRuleIndex())
+        val msg = "Rule " + ruleName + " " + e.getMessage()
+        parser.notifyErrorListeners(e.getOffendingToken(), msg, e)
+    }
+
 }
