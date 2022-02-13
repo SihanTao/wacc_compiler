4b39dbc29aee41bcf56a31c60045915bc7e9402a
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-02-13 04:14:51 +0000

    Use boolean instead of node in WACCSyntaxErrorVisitor

--- src/errorHandler/WACCSyntaxErrorStrategy.kt
+++ src/errorHandler/WACCSyntaxErrorStrategy.kt
@@ -5,11 +5,12 @@ import org.antlr.v4.runtime.Parser
 import org.antlr.v4.runtime.RecognitionException
 
 class WACCSyntaxErrorStrategy : DefaultErrorStrategy() {
-    override fun reportError(parser: Parser, e: RecognitionException) {
-        if (e.message == null) {
-            parser.notifyErrorListeners(e.offendingToken, "error", e)
-        } else {
-            parser.notifyErrorListeners(e.offendingToken, e.message, e)
-        }
-    }
+//    override fun reportError(parser: Parser, e: RecognitionException) {
+//        if (e.message == null) {
+//            parser.notifyErrorListeners(e.offendingToken, "error", e)
+//        } else {
+//            parser.notifyErrorListeners(e.offendingToken, e.message, e)
+//        }
+//    }
+//    return
 }
