a30cc800deaa53daa2864faace73e3827ddd70a5
st2220 <sihan.tao@imperial.ac.uk>
2022-02-25 23:06:51 +0000

    Add class Code for the section begin with .global main directive

---
+++ src/backend/Code.java
@@ -0,0 +1,24 @@
+package backend;
+
+import backend.instructions.Instruction;
+
+import java.util.List;
+
+public class Code implements Directive {
+
+  private final List<Instruction> instructions;
+
+  public Code(List<Instruction> instructions) {
+    this.instructions = instructions;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("\t.global main");
+    for (Instruction instruction : instructions) {
+      stringBuilder.append(instruction.toString());
+    }
+    return stringBuilder.toString();
+  }
+}
