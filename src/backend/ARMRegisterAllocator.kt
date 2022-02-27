73e6e5ab021d3b3261b01e3813fdc15a6e8a133b
st2220 <sihan.tao@imperial.ac.uk>
2022-02-27 19:59:00 +0000

    Use companion object to make the methods static.

--- src/backend/ARMRegisterAllocator.kt
+++ src/backend/ARMRegisterAllocator.kt
@@ -8,36 +8,37 @@ class ARMRegisterAllocator() {
         // in refCompile
         val GENERAL_REG_START = 4
         val GENERAL_REG_END = 12
-    }
-
-    private val registers: List<ARMRegister> = listOf(
-        ARMRegister.R0,
-        ARMRegister.R1,
-        ARMRegister.R2,
-        ARMRegister.R3,
-        ARMRegister.R4,
-        ARMRegister.R5,
-        ARMRegister.R6,
-        ARMRegister.R7,
-        ARMRegister.R8,
-        ARMRegister.R9,
-        ARMRegister.R10,
-        ARMRegister.R11,
-        ARMRegister.R12,
-        ARMRegister.SP,
-        ARMRegister.LR,
-        ARMRegister.PC)
-
-    var counter = GENERAL_REG_START
-
-    fun allocate(): ARMRegister {
-        if (counter < GENERAL_REG_START || counter > GENERAL_REG_END) {
-            throw IllegalArgumentException("Can't allocate register: R$counter")
+        var counter = GENERAL_REG_START
+        fun allocate(): ARMRegister {
+            if (counter < GENERAL_REG_START || counter > GENERAL_REG_END) {
+                throw IllegalArgumentException("Can't allocate register: R$counter")
+            }
+            return registers[counter++]
         }
-        return registers[counter++]
+
+        fun free(): ARMRegister {
+            return registers[counter--]
+        }
+
+        private val registers: List<ARMRegister> = listOf(
+            ARMRegister.R0,
+            ARMRegister.R1,
+            ARMRegister.R2,
+            ARMRegister.R3,
+            ARMRegister.R4,
+            ARMRegister.R5,
+            ARMRegister.R6,
+            ARMRegister.R7,
+            ARMRegister.R8,
+            ARMRegister.R9,
+            ARMRegister.R10,
+            ARMRegister.R11,
+            ARMRegister.R12,
+            ARMRegister.SP,
+            ARMRegister.LR,
+            ARMRegister.PC
+        )
     }
 
-    fun free(): ARMRegister {
-        return registers[counter--]
-    }
+
 }
