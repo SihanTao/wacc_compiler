bee1c183b2553bf3fc4bd3c8c57d3aa675ae81ad
ayj20 <ayj20@ic.ac.uk>
2022-02-27 15:30:25 +0000

    added more functionality to backend testing
    -parsing refCompile generated file to compare right part
    -add ability to supply input
    -add ability to test single file
    -add ability to test files in supplied directory

--- backEndTest.sh
+++ backEndTest.sh
@@ -21,25 +21,68 @@ VALID_TEST=$(find ${VALID_EXAMPLES} -name "*.wacc")
 TOTAL_COUNT=0
 PASSED=0
 
+INPUT="c"
+SINGLE_FILE=""
+DIRECTORY=""
+
+# optional flags:
+# -i {input}, sets input for the programs
+# -f {file}, test only file supplied
+# -d {directory}, tests all files in directory supplied
+# automatically searches for file/directory name, no need to put whole path
+while getopts 'i:f:d:' flag; do
+  case "${flag}" in
+    i) INPUT="${OPTARG}" ;;
+    f) SINGLE_FILE="${OPTARG}" ;;
+    d) DIRECTORY="${OPTARG}" ;;
+  esac
+done
+
 echo "=============================Valid Case Test============================================"
 echo "Correct Program must have expected output matches the execute output"
 echo "========================================================================================"
 
-for file in $VALID_TEST; do
+if [[ $SINGLE_FILE -ne "" ]]; then
+  testFile($(find ${VALID_EXAMPLES} -name $SINGLE_FILE))
+elif [[ $DIRECTORY -ne "" ]]; then
+  for file in $(find "${VALID_EXAMPLES}/$DIRECTORY" -name "*.wacc"); do
+    testFile(file)
+  done
+else
+  for file in $VALID_TEST; do
+    testFile(file)
+  done
+fi
 
+testFile() {
+
+    file=$1
     NAME=$(basename -s .wacc "${file}")
 
     echo "testing $NAME"
 
+    # generate our output
     ./compile $file > "$OUT_DIR/$NAME.s"
-    arm-linux-gnueabi-gcc -o "$NAME_11.s" -mcpu=arm1176jzf-s -mtune=arm1176jzf-s "$OUT_DIR/$NAME.s" > "$OUT_DIR/$NAME_11.s"
-    qemu-arm -L /usr/arm-linux-gnueabi/ "$OUT_DIR/$NAME_11.s" > "$OUT_DIR/$NAME.out"
+    arm-linux-gnueabi-gcc -o "$OUT_DIR/$NAME_11.s" -mcpu=arm1176jzf-s -mtune=arm1176jzf-s "$OUT_DIR/$NAME.s"
+    echo $INPUT | qemu-arm -L /usr/arm-linux-gnueabi/ "$OUT_DIR/$NAME_11.s" > "$OUT_DIR/$NAME.out"
 
-    $REFERENCE_COMPILER $file > "$OUT_DIR/${NAME}_REF.s"
-    $REFERENCE_EMULATOR "$OUT_DIR/${NAME}_REF.s" > "$OUT_DIR/${NAME}_REF.out"
+    # generate reference file
+    echo $INPUT | $REFERENCE_COMPILER $file -a -x > "$OUT_DIR/${NAME}_REF.txt"
+
+    # get the asm and output from the generated file
+    awk '/===========================================================/{n++}
+      {
+        if ( n == 1 )
+          print >"out" tmp ".s";
+        else if ( n == 3)
+          print > "out" tmp ".out";
+      }' "$OUT_DIR/${NAME}_REF.txt"
+    sed -i '1d' tmp.s && mv tmp.s "$OUT_DIR/${NAME}_REF.s"
+    sed -i '1d' tmp.out && mv tmp.out "$OUT_DIR/${NAME}_REF.out"
 
     let COUNTER+=1
     
+    # compare our output and reference output
     if cmp -s "$OUT_DIR/$NAME.out" "$OUT_DIR/${NAME}_REF.out"; then
       echo "PASSED"
       let PASSED+=1
@@ -47,8 +90,8 @@ for file in $VALID_TEST; do
       echo "FAILED"
       diff "$OUT_DIR/$NAME.out" "$OUT_DIR/${NAME}_REF.out" > "$FAILS_DIR/$NAME.txt"
     fi
-done
 
+}
 
 echo "================================TEST RESULT============================================="
 echo "Total Test: $TOTAL_COUNT"
