d96a508a8c1b7fa17cea1acfea4b82ade64e3c70
st2220 <sihan.tao@imperial.ac.uk>
2022-02-27 19:03:52 +0000

    Fix the problem that the MOV is not correctly initialized.

--- simpleScript.sh
+++ simpleScript.sh
@@ -1,13 +1,16 @@
-FILENAME="comment"
+FILENAME="exitWrap"
+TEST_FOLDER_PATH="test/wacc_examples/valid/basic/exit"
 
-make clean
-make
+#make clean
+#make
 
-echo "Compiling: src/wacc_examples/valid/basic/skip/${FILENAME}.wacc"
-./compile "src/wacc_examples/valid/basic/skip/${FILENAME}.wacc"
+echo "Compiling: ${TEST_FOLDER_PATH}/${FILENAME}.wacc"
+./compile "${TEST_FOLDER_PATH}/${FILENAME}.wacc"
 
 echo "Creating exec file ${FILENAME}.s ......"
 arm-linux-gnueabi-gcc -o "$FILENAME" -mcpu=arm1176jzf-s -mtune=arm1176jzf-s "${FILENAME}.s"
 
 echo "Executing the exec file ${FILENAME}.s ......"
 qemu-arm -L /usr/arm-linux-gnueabi/ $FILENAME
+
+echo "The exit code is $?"
--- src/backend/instructions/Mov.kt
+++ src/backend/instructions/Mov.kt
@@ -3,14 +3,11 @@ package backend.instructions
 import backend.ARMRegister
 import backend.instructions.operand.Operand2
 
-class Mov(Rd: ARMRegister, operand2: Operand2) : Instruction {
+class Mov(val Rd: ARMRegister, val operand2: Operand2) : Instruction {
     /*
     * MOV{S}{cond} Rd, Operand2
     * MOV{cond} Rd, #imm16
     * */
-
-    private val Rd: ARMRegister? = null
-    private val operand2: Operand2? = null
 
     override fun toString(): String {
         return "MOV $Rd, $operand2"
