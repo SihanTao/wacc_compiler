f39159f767853b61756a987d4283b826f760b820
st2220 <sihan.tao@imperial.ac.uk>
2022-02-27 19:55:03 +0000

    Add a class for register allocation.

--- src/backend/ARMRegister.kt
+++ src/backend/ARMRegister.kt
@@ -3,7 +3,7 @@ package backend
 import java.util.*
 
 enum class ARMRegister {
-    R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10,R11, R12,
+    R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12,
     SP, // Stack pointer R13
     LR, // Link register R14
     PC; // Program Counter R15
---
+++ src/backend/ARMRegisterAllocator.kt
@@ -0,0 +1,43 @@
+package backend
+
+import java.lang.IllegalArgumentException
+
+class ARMRegisterAllocator() {
+    companion object {
+        // R0-R12 is the general purpose register. start from R4 to be the same
+        // in refCompile
+        val GENERAL_REG_START = 4
+        val GENERAL_REG_END = 12
+    }
+
+    val registers: List<ARMRegister> = listOf(
+        ARMRegister.R0,
+        ARMRegister.R1,
+        ARMRegister.R2,
+        ARMRegister.R3,
+        ARMRegister.R4,
+        ARMRegister.R5,
+        ARMRegister.R6,
+        ARMRegister.R7,
+        ARMRegister.R8,
+        ARMRegister.R9,
+        ARMRegister.R10,
+        ARMRegister.R11,
+        ARMRegister.R12,
+        ARMRegister.SP,
+        ARMRegister.LR,
+        ARMRegister.PC)
+
+    var counter = GENERAL_REG_START
+
+    fun allocate(): ARMRegister {
+        if (counter < GENERAL_REG_START || counter > GENERAL_REG_END) {
+            throw IllegalArgumentException("Can't allocate register: R$counter")
+        }
+        return registers[counter++]
+    }
+
+    fun free(): ARMRegister {
+        return registers[counter--]
+    }
+}
