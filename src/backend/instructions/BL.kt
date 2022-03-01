243eee985fa29cf719669405b9200f11cf39065f
st2220 <sihan.tao@imperial.ac.uk>
2022-03-01 21:07:33 +0000

    Add a new constructor for BL

--- src/backend/instructions/BL.kt
+++ src/backend/instructions/BL.kt
@@ -4,6 +4,8 @@ import backend.Cond
 
 class BL(private val cond: Cond, val label: String) : Instruction {
     // BL {cond} Label
+
+    constructor(label: String): this(Cond.NONE, label)
     override fun toString(): String {
         return "BL $label"
     }
