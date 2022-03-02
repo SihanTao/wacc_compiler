e7f3d8004924bcf80d378a314314af8227d86577
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 18:20:37 +0000

    Refactor PrintBool

--- src/instruction/waccLibrary/PrintBool.kt
+++ src/instruction/waccLibrary/PrintBool.kt
@@ -5,30 +5,24 @@ import instruction.*
 import instruction.addressing_mode.StaticRef
 import register.Register
 
-class PrintBool(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
-    private val instructions: List<ARM11Instruction>
-    private val dependencies: List<WACCLibraryFunction>
+class PrintBool : WACCLibraryFunction() {
+    private val dependencies: List<WACCLibraryFunction> = listOf()
 
-    init {
+    override fun getInstructions(codeGenerator: WACCCodeGenerator): List<ARM11Instruction> {
         val msg1 = codeGenerator.addDataElement("true\\0")
         val msg2 = codeGenerator.addDataElement("false\\0")
-        instructions = listOf(
+        return listOf(
                 LABEL("p_print_bool"),
                 PUSH(Register.LR),
                 CMP(Register.R0, 0),
-                LDR(Register.R0, StaticRef("msg_$msg1"), cond=Cond.NE),
-                LDR(Register.R0, StaticRef("msg_$msg2"), cond=Cond.NE),
+                LDR(Register.R0, StaticRef("msg_$msg1")).on(Cond.NE),
+                LDR(Register.R0, StaticRef("msg_$msg2")).on(Cond.EQ),
                 ADD(Register.R0, Register.R0, 4),
-                B(B.Mode.LINK, "printf"),
+                BL("printf"),
                 MOV(Register.R0, 0),
-                B(B.Mode.LINK, "fflush"),
+                BL( "fflush"),
                 POP(Register.PC),
         )
-        dependencies = listOf()
-    }
-
-    override fun getInstructions(): List<ARM11Instruction> {
-        return instructions
     }
 
     override fun getDependencies(): List<WACCLibraryFunction> {
