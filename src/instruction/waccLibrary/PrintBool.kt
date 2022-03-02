4f612e9ea8eeb95e63086e5697aba28130bb9d5f
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 10:48:51 +0000

    Implement PrintBool

--- src/instruction/waccLibrary/PrintBool.kt
+++ src/instruction/waccLibrary/PrintBool.kt
@@ -1,11 +1,32 @@
 package instruction.waccLibrary
 
-import instruction.ARM11Instruction
+import WACCCodeGenerator
+import instruction.*
+import instruction.addrMode2.StaticRef
+import org.antlr.v4.codegen.CodeGenerator
+import register.ARM11Register
 
-class PrintBool(dataTable: Map<String, Int>) : WACCLibraryFunction(dataTable) {
-    private val instructions: List<ARM11Instruction> = TODO()
-    private val dependencies: List<WACCLibraryFunction> = TODO()
+class PrintBool(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
+    private val instructions: List<ARM11Instruction>
+    private val dependencies: List<WACCLibraryFunction>
 
+    init {
+        val msg1 = codeGenerator.addDataElement("true\\0")
+        val msg2 = codeGenerator.addDataElement("false\\0")
+        instructions = listOf(
+                Label("p_print_bool"),
+                Push(ARM11Register.LR),
+                Compare(ARM11Register.R0, 0),
+                Load(ARM11Register.R0, StaticRef("msg_$msg1"), cond=Cond.NE),
+                Load(ARM11Register.R0, StaticRef("msg_$msg2"), cond=Cond.NE),
+                Add(ARM11Register.R0, ARM11Register.R0, 4),
+                Branch(Branch.Mode.LINK, "printf"),
+                Move(ARM11Register.R0, 0),
+                Branch(Branch.Mode.LINK, "fflush"),
+                Pop(ARM11Register.PC),
+        )
+        dependencies = listOf()
+    }
 
     override fun getInstructions(): List<ARM11Instruction> {
         return instructions
