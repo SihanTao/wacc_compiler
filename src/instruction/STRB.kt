baf5b3f052d3faa8bd783d983de4bd01bdc6cd15
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 13:36:14 +0000

    Add STRB

--- src/instruction/STRB.kt
+++ src/instruction/STRB.kt
@@ -1,4 +1,19 @@
 package instruction
 
-class STRB {
+import instruction.addressing_mode.AddressingMode
+import register.Register
+
+class STRB(val rd: Register,
+          val addr: AddressingMode,
+          var cond: Cond = Cond.AL): ARM11Instruction
+{
+
+    fun on(cond: Cond): STRB {
+        this.cond = cond
+        return this
+    }
+
+    override fun toString(): String {
+        return "STR${cond}B $rd, $addr"
+    }
 }
