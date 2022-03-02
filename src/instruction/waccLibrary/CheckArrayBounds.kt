caa663de9ae2ad9a854ce8ee7b87f51acb225857
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 11:18:43 +0000

    Implement CheckArrayBounds

--- src/instruction/waccLibrary/CheckArrayBounds.kt
+++ src/instruction/waccLibrary/CheckArrayBounds.kt
@@ -1,10 +1,33 @@
 package instruction.waccLibrary
 
-import instruction.ARM11Instruction
+import WACCCodeGenerator
+import WACCCodeGeneratorVisitor
+import instruction.*
+import instruction.addrMode2.ImmOffset
+import instruction.addrMode2.RegOffset
+import instruction.addrMode2.StaticRef
+import register.ARM11Register
 
-class CheckArrayBounds(dataTable: Map<String, Int>) : WACCLibraryFunction(dataTable) {
-    private val instructions: List<ARM11Instruction> = TODO()
-    private val dependencies: List<WACCLibraryFunction> = TODO()
+class CheckArrayBounds(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
+    private val instructions: List<ARM11Instruction>
+    private val dependencies: List<WACCLibraryFunction>
+
+    init {
+        val msgCode1 = codeGenerator.addDataElement("ArrayIndexOutOfBoundsError: negative index\\n\\0")
+        val msgCode2 = codeGenerator.addDataElement("ArrayIndexOutOfBoundsError: index too large\\n\\0")
+        instructions = listOf(
+                Label("p_check_array_bounds"),
+                Push(ARM11Register.LR),
+                Compare(ARM11Register.R0, 0),
+                Load(ARM11Register.R0, StaticRef("msg_$msgCode1"), cond= Cond.LT),
+                Branch(Branch.Mode.LINK, "p_throw_runtime_error", Cond.LT),
+                Load(ARM11Register.R1, ImmOffset(ARM11Register.R1)),
+                Load(ARM11Register.R0, StaticRef("msg_$msgCode2"), cond= Cond.CS),
+                Branch(Branch.Mode.LINK, "p_throw_runtime_error", Cond.CS),
+                Pop(ARM11Register.PC),
+        )
+        dependencies = listOf(ThrowRuntimeError(codeGenerator))
+    }
 
 
     override fun getInstructions(): List<ARM11Instruction> {
