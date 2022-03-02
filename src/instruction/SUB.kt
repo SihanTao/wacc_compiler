2b2cfdc2d55975a61bd09097552ac0a093d87e5a
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 13:38:40 +0000

    Refactor/Reimplement SUB for easier calling

---
+++ src/instruction/SUB.kt
@@ -0,0 +1,32 @@
+package instruction
+
+import instruction.shifter_operand.*
+import register.Register
+
+class SUB(val rd: register.Register, val rn: register.Register,
+          val Op: ShifterOperand,
+          var cond: Cond = Cond.AL,
+          val S: Boolean = false,
+):ARM11Instruction {
+    constructor(rd: Register, rn: Register, rm: Register):
+            this(rd=rd, rn=rn, Op= Reg(rm))
+
+    constructor(rd: Register, rn: Register, Imm: Int):
+            this(rd=rd, rn=rn, Op=Imm(Imm))
+
+    constructor(rd: Register, rn: Register, rm: Register, shift: Shift, imm: Int):
+            this(rd=rd, rn=rn, Op= ShiftImm(rm, shift, imm))
+
+    constructor(rd: Register, rn: Register, rm: Register, shift: Shift, rs: Register):
+            this(rd=rd, rn=rn, Op= ShiftReg(rm, shift, rs))
+
+    fun on(cond: Cond): SUB {
+        this.cond = cond
+        return this
+    }
+
+    override fun toString(): String {
+        val s = if (S) "S" else ""
+        return "SUB$cond${s} $rd, $rn, $Op"
+    }
+}
