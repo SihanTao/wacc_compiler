adc93a87913c132bdcdb2434bb24abe36ebde9bf
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 13:33:55 +0000

    Rename PUSH

--- src/instruction/PUSH.kt
+++ src/instruction/PUSH.kt
@@ -2,7 +2,7 @@ package instruction
 
 import register.Register
 
-class Push(val Register: Register): ARM11Instruction {
+class PUSH(val Register: Register): ARM11Instruction {
     override fun toString(): String {
         return "PUSH {$Register}"
     }
