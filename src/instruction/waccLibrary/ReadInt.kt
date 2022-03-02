8adab91667a0f08fbd389ba4ca2d8fd25fc84a36
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 11:23:11 +0000

    Implement ReadInt

--- src/instruction/waccLibrary/ReadInt.kt
+++ src/instruction/waccLibrary/ReadInt.kt
@@ -1,10 +1,27 @@
 package instruction.waccLibrary
 
-import instruction.ARM11Instruction
+import WACCCodeGenerator
+import instruction.*
+import instruction.addrMode2.StaticRef
+import register.ARM11Register
 
-class ReadInt(dataTable: Map<String, Int>) : WACCLibraryFunction(dataTable) {
-    private val instructions: List<ARM11Instruction> = TODO()
-    private val dependencies: List<WACCLibraryFunction> = TODO()
+class ReadInt(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
+    private val instructions: List<ARM11Instruction>
+    private val dependencies: List<WACCLibraryFunction>
+
+    init {
+        val msgCode = codeGenerator.addDataElement("%d\\0")
+        instructions = listOf(
+                Label("p_read_int"),
+                Push(ARM11Register.LR),
+                Move(ARM11Register.R1, ARM11Register.R0),
+                Load(ARM11Register.R0, StaticRef("msg_$msgCode")),
+                Add(ARM11Register.R0, ARM11Register.R0, 4),
+                Branch(Branch.Mode.LINK, "scanf"),
+                Pop(ARM11Register.PC)
+        )
+        dependencies = listOf()
+    }
 
 
     override fun getInstructions(): List<ARM11Instruction> {
