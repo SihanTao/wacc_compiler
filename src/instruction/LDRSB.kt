42e6147596ebc4ceae55ff4241bcd7a789d33ff0
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 13:24:49 +0000

    Refactor/Reimplement LDRSB for easier calling

--- src/instruction/LDRSB.kt
+++ src/instruction/LDRSB.kt
@@ -1,4 +1,22 @@
 package instruction
 
-class LDRSB {
+import instruction.addressing_mode.AddressingMode
+import instruction.addressing_mode.StaticRef
+import register.Register
+
+class LDRSB(val rd: Register,
+           val addr: AddressingMode,
+           var cond: Cond = Cond.AL,): ARM11Instruction
+{
+    constructor(rd: Register, `=`: String):
+            this(rd, StaticRef(`=`))
+
+    fun on(cond: Cond): LDRSB {
+        this.cond = cond
+        return this
+    }
+
+    override fun toString(): String {
+        return "LDR${cond}SB $rd, $addr"
+    }
 }
