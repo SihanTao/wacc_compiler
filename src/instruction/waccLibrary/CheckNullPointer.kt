757b20b3cba68ded593abf538bb20083e440836b
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 20:43:26 +0000

    Refactor CheckNullPointer

--- src/instruction/waccLibrary/CheckNullPointer.kt
+++ src/instruction/waccLibrary/CheckNullPointer.kt
@@ -5,26 +5,19 @@ import instruction.*
 import instruction.addressing_mode.StaticRef
 import register.Register
 
-class CheckNullPointer(codeGenerator: WACCCodeGenerator): WACCLibraryFunction(codeGenerator) {
-    private val instructions: List<ARM11Instruction>
-    private val dependencies: List<WACCLibraryFunction>
+class CheckNullPointer: WACCLibraryFunction() {
+    private val dependencies: List<WACCLibraryFunction> = listOf(ThrowRuntimeError())
 
-    init {
+    override fun getInstructions(codeGenerator:WACCCodeGenerator): List<ARM11Instruction> {
         val msgCode = codeGenerator.addDataElement("NullReferenceError: dereference a null reference\\n\\0")
-        instructions = listOf(
+        return listOf(
                 LABEL("p_check_null_pointer"),
                 PUSH(Register.LR),
                 CMP(Register.R0, 0),
-                LDR(Register.R0, StaticRef("msg_$msgCode"), cond= Cond.EQ),
-                B(B.Mode.LINK, "p_throw_runtime_error", Cond.EQ),
+                LDR(Register.R0, StaticRef("msg_$msgCode"), cond=Cond.EQ),
+                BL("p_throw_runtime_error", Cond.EQ),
                 POP(Register.PC),
         )
-        dependencies = listOf(ThrowRuntimeError(codeGenerator))
-    }
-
-
-    override fun getInstructions(): List<ARM11Instruction> {
-        return instructions
     }
 
     override fun getDependencies(): List<WACCLibraryFunction> {
