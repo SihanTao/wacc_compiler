a9d401d8abafa6a4c4998f73c22305d5f0abddc4
st2220 <sihan.tao@imperial.ac.uk>
2022-03-02 13:42:35 +0000

    Create a new class to represent RSBS

---
+++ src/backend/instructions/unopAndBinop/RSBS.kt
@@ -0,0 +1,18 @@
+package backend.instructions.unopAndBinop
+
+import backend.ARMRegister
+import backend.instructions.Instruction
+import backend.instructions.operand.Operand2
+
+class RSBS(
+    private val Rd: ARMRegister,
+    private val Rn: ARMRegister,
+    private val operand2: Operand2
+) : Instruction {
+
+    constructor(Rd: ARMRegister, Rn: ARMRegister, value: Int) : this(
+        Rd,
+        Rn,
+        Operand2(value)
+    )
+}
