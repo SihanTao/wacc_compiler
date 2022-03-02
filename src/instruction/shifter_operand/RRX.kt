f62d68353108d41ee8c35c2209ba2f3d516e42d7
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 12:54:46 +0000

    Add interface for ShiftImm and RRX

--- src/instruction/shifter_operand/RRX.kt
+++ src/instruction/shifter_operand/RRX.kt
@@ -2,7 +2,7 @@ package instruction.shifter_operand
 
 import register.Register
 
-class RRX(val Rm: Register) {
+class RRX(val Rm: Register): ShifterOperand {
     override fun toString(): String {
         return "$Rm, RRX"
     }
--- src/instruction/shifter_operand/ShiftImm.kt
+++ src/instruction/shifter_operand/ShiftImm.kt
@@ -2,7 +2,7 @@ package instruction.shifter_operand
 
 import register.Register
 
-class ShiftImm(val Rm: Register, val shift: Shift, val imm: Int) {
+class ShiftImm(val Rm: Register, val shift: Shift, val imm: Int): ShifterOperand {
     override fun toString(): String {
         return "$Rm, $shift #$imm"
     }
