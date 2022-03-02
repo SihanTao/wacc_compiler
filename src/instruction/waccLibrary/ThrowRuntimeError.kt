0cd7c071d6135e0d0d1b55030ea5e0e9728ddff3
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 11:11:24 +0000

    Implement ThrowRuntimeError

--- src/instruction/waccLibrary/ThrowRuntimeError.kt
+++ src/instruction/waccLibrary/ThrowRuntimeError.kt
@@ -1,11 +1,24 @@
 package instruction.waccLibrary
 
-import instruction.ARM11Instruction
+import WACCCodeGenerator
+import instruction.*
+import instruction.addrMode2.Sign
+import instruction.addrMode2.StaticRef
+import register.ARM11Register
 
-class ThrowRuntimeError(dataTable: Map<String, Int>) : WACCLibraryFunction(dataTable) {
-    private val instructions: List<ARM11Instruction> = TODO()
-    private val dependencies: List<WACCLibraryFunction> = TODO()
+class ThrowRuntimeError(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
+    private val instructions: List<ARM11Instruction>
+    private val dependencies: List<WACCLibraryFunction>
 
+    init {
+        instructions = listOf(
+                Label("p_throw_runtime_error"),
+                Branch(Branch.Mode.LINK, "p_print_string"),
+                Move(ARM11Register.R0, -1),
+                Branch(Branch.Mode.LINK, "exit")
+        )
+        dependencies = listOf(PrintString(codeGenerator))
+    }
 
     override fun getInstructions(): List<ARM11Instruction> {
         return instructions
