a94823617bfdee88d731625acfecf54e1a73201d
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 20:37:37 +0000

    Refactor PrintReference

--- src/instruction/waccLibrary/PrintReference.kt
+++ src/instruction/waccLibrary/PrintReference.kt
@@ -5,29 +5,23 @@ import instruction.*
 import instruction.addressing_mode.StaticRef
 import register.Register
 
-class PrintReference(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
-    private val instructions: List<ARM11Instruction>
-    private val dependencies: List<WACCLibraryFunction>
+class PrintReference: WACCLibraryFunction() {
+    private val dependencies: List<WACCLibraryFunction> = listOf()
 
-    init {
+
+    override fun getInstructions(codeGenerator: WACCCodeGenerator): List<ARM11Instruction> {
         val msgCode = codeGenerator.addDataElement("%p\\0")
-        instructions = listOf(
+        return listOf(
                 LABEL("p_print_reference"),
                 PUSH(Register.LR),
                 MOV(Register.R1, Register.R0),
                 LDR(Register.R0, StaticRef("msg_$msgCode")),
                 ADD(Register.R0, Register.R0, 4),
-                B(B.Mode.LINK, "printf"),
+                BL("printf"),
                 MOV(Register.R0, 0),
-                B(B.Mode.LINK, "fflush"),
+                BL("fflush"),
                 POP(Register.PC),
         )
-        dependencies = listOf()
-    }
-
-
-    override fun getInstructions(): List<ARM11Instruction> {
-        return instructions
     }
 
     override fun getDependencies(): List<WACCLibraryFunction> {
