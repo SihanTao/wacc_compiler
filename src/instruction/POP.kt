c185f7c465be9662b917c11bb2e64bf6410bac55
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 13:33:33 +0000

    Rename POP

--- src/instruction/POP.kt
+++ src/instruction/POP.kt
@@ -3,7 +3,7 @@ package instruction.waccLibrary
 import instruction.ARM11Instruction
 import register.Register
 
-class Pop(val Register: Register): ARM11Instruction {
+class POP(val Register: Register): ARM11Instruction {
     override fun toString(): String {
         return "PUSH {$Register}"
     }
