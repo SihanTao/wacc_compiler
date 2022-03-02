cc8e15a6fee6c72ae598c9155fa801bdc0d683e0
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 13:05:00 +0000

    Add BL

--- src/instruction/BL.kt
+++ src/instruction/BL.kt
@@ -1,4 +1,16 @@
 package instruction
 
-class BL {
+class BL(
+        val addr: String,
+        var cond: Cond = Cond.AL
+): ARM11Instruction {
+
+    fun on(cond: Cond): BL {
+        this.cond = cond
+        return this
+    }
+
+    override fun toString(): String {
+        return "BL$cond $addr"
+    }
 }
