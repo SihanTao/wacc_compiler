e83deba87d9ed09c2d65366e07a333df4ff64f1d
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 11:32:00 +0000

    Implement CheckNullPointer

--- src/instruction/waccLibrary/CheckNullPointer.kt
+++ src/instruction/waccLibrary/CheckNullPointer.kt
@@ -1,10 +1,27 @@
 package instruction.waccLibrary
 
-import instruction.ARM11Instruction
+import WACCCodeGenerator
+import instruction.*
+import instruction.addrMode2.StaticRef
+import org.antlr.v4.codegen.CodeGenerator
+import register.ARM11Register
 
-class CheckNullPointer: WACCLibraryFunction() {
-    private val instructions: List<ARM11Instruction> = TODO()
-    private val dependencies: List<WACCLibraryFunction> = TODO()
+class CheckNullPointer(codeGenerator: WACCCodeGenerator): WACCLibraryFunction(codeGenerator = ) {
+    private val instructions: List<ARM11Instruction>
+    private val dependencies: List<WACCLibraryFunction>
+
+    init {
+        val msgCode = codeGenerator.addDataElement("NullReferenceError: dereference a null reference\\n\\0")
+        instructions = listOf(
+                Label("p_check_null_pointer"),
+                Push(ARM11Register.LR),
+                Compare(ARM11Register.R0, 0),
+                Load(ARM11Register.R0, StaticRef("msg_$msgCode"), cond= Cond.EQ),
+                Branch(Branch.Mode.LINK, "p_throw_runtime_error", Cond.EQ),
+                Pop(ARM11Register.PC),
+        )
+        dependencies = listOf(ThrowRuntimeError(codeGenerator))
+    }
 
 
     override fun getInstructions(): List<ARM11Instruction> {
