7333b2a43a345106a1bd588e8dbe4c97920f47b9
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 12:57:31 +0000

    Refactor/Reimplement AND for easier calling

---
+++ src/instruction/AND.kt
@@ -0,0 +1,34 @@
+package instruction
+
+import instruction.shifter_operand.*
+import register.Register
+
+class AND(val rd: Register, val rn: Register,
+          val Op: ShifterOperand,
+          var cond: Cond = Cond.AL,
+          val S: Boolean = false
+):ARM11Instruction {
+    constructor(rd: Register, rn: Register, rm: Register):
+            this(rd=rd, rn=rn, Op= Reg(rm))
+
+    constructor(rd: Register, rn: Register, Imm: Int):
+            this(rd=rd, rn=rn, Op=Imm(Imm))
+
+    constructor(rd: Register, rn: Register, rm: Register, shift: Shift, imm: Int):
+            this(rd=rd, rn=rn, Op= ShiftImm(rm, shift, imm))
+
+    constructor(rd: Register, rn: Register, rm: Register, shift: Shift, rs: Register):
+            this(rd=rd, rn=rn, Op= ShiftReg(rm, shift, rs))
+
+    fun on(cond: Cond): AND {
+        this.cond = cond
+        return this
+    }
+
+    override fun toString(): String {
+        val s = if (S) "S" else ""
+        return "AND$cond${s} $rd, $rn, $Op"
+    }
+
+
+}
