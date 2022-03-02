d103ec93feab15b7a2f2bbb365e7f4e9441fb7a3
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 11:41:57 +0000

    Implement FreePair

--- src/instruction/waccLibrary/FreePair.kt
+++ src/instruction/waccLibrary/FreePair.kt
@@ -1,10 +1,36 @@
 package instruction.waccLibrary
 
-import instruction.ARM11Instruction
+import WACCCodeGenerator
+import instruction.*
+import instruction.addrMode2.ImmOffset
+import instruction.addrMode2.Sign
+import instruction.addrMode2.StaticRef
+import instruction.shiftOperand.Immediate
+import register.ARM11Register
 
-class FreePair(dataTable: Map<String, Int>) : WACCLibraryFunction(dataTable) {
-    private val instructions: List<ARM11Instruction> = TODO()
-    private val dependencies: List<WACCLibraryFunction> = TODO()
+class FreePair(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
+    private val instructions: List<ARM11Instruction>
+    private val dependencies: List<WACCLibraryFunction>
+
+    init {
+        val msgCode = codeGenerator.addDataElement("NullReferenceError: dereference a null reference\\n\\0")
+        instructions = listOf(
+                Label("p_free_pair"),
+                Push(ARM11Register.LR),
+                Compare(ARM11Register.R0, 0),
+                Load(ARM11Register.R0, StaticRef("msg_$msgCode")),
+                Branch("p_throw_runtime_error", cond=Cond.EQ),
+                Load(ARM11Register.R0, ImmOffset(ARM11Register.R0)),
+                Branch(Branch.Mode.LINK, "free"),
+                Load(ARM11Register.R0, ImmOffset(ARM11Register.SP)),
+                Load(ARM11Register.R0, ImmOffset(ARM11Register.R0, Pair(Sign.PLUS, 4))),
+                Branch(Branch.Mode.LINK, "free"),
+                Pop(ARM11Register.R0),
+                Branch(Branch.Mode.LINK, "free"),
+                Pop(ARM11Register.PC)
+        )
+        dependencies = listOf(ThrowRuntimeError(codeGenerator))
+    }
 
 
     override fun getInstructions(): List<ARM11Instruction> {
