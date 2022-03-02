51f002f6c256ebe3564cd359f14ccf4149c0a337
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 20:41:40 +0000

    Refactor FreePair

--- src/instruction/waccLibrary/FreePair.kt
+++ src/instruction/waccLibrary/FreePair.kt
@@ -7,33 +7,27 @@ import instruction.addressing_mode.Sign
 import instruction.addressing_mode.StaticRef
 import register.Register
 
-class FreePair(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
-    private val instructions: List<ARM11Instruction>
-    private val dependencies: List<WACCLibraryFunction>
+class FreePair : WACCLibraryFunction() {
+    private val dependencies: List<WACCLibraryFunction> = listOf(ThrowRuntimeError())
 
-    init {
+    override fun getInstructions(codeGenerator: WACCCodeGenerator): List<ARM11Instruction> {
         val msgCode = codeGenerator.addDataElement("NullReferenceError: dereference a null reference\\n\\0")
-        instructions = listOf(
+        return listOf(
                 LABEL("p_free_pair"),
                 PUSH(Register.LR),
                 CMP(Register.R0, 0),
-                LDR(Register.R0, StaticRef("msg_$msgCode")),
+                LDR(Register.R0, StaticRef("msg_$msgCode")).on(Cond.EQ),
                 B("p_throw_runtime_error", cond=Cond.EQ),
+                PUSH(Register.R0),
                 LDR(Register.R0, ImmOffset(Register.R0)),
-                B(B.Mode.LINK, "free"),
+                BL("free"),
                 LDR(Register.R0, ImmOffset(Register.SP)),
                 LDR(Register.R0, ImmOffset(Register.R0, Pair(Sign.PLUS, 4))),
-                B(B.Mode.LINK, "free"),
+                BL("free"),
                 POP(Register.R0),
-                B(B.Mode.LINK, "free"),
+                BL("free"),
                 POP(Register.PC)
         )
-        dependencies = listOf(ThrowRuntimeError(codeGenerator))
-    }
-
-
-    override fun getInstructions(): List<ARM11Instruction> {
-        return instructions
     }
 
     override fun getDependencies(): List<WACCLibraryFunction> {
