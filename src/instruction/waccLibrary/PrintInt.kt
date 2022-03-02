cf1cf0422ecc759a79524f2ef55938c20e61bb26
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 10:58:43 +0000

    implemeny throwOverflowError

--- src/instruction/waccLibrary/PrintInt.kt
+++ src/instruction/waccLibrary/PrintInt.kt
@@ -1,10 +1,29 @@
 package instruction.waccLibrary
 
-import instruction.ARM11Instruction
+import WACCCodeGenerator
+import instruction.*
+import instruction.addrMode2.StaticRef
+import register.ARM11Register
 
-class PrintInt(dataTable: Map<String, Int>) : WACCLibraryFunction(dataTable) {
-    private val instructions: List<ARM11Instruction> = TODO()
-    private val dependencies: List<WACCLibraryFunction> = TODO()
+class PrintInt(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
+    private val instructions: List<ARM11Instruction>
+    private val dependencies: List<WACCLibraryFunction>
+
+    init {
+        val msg = codeGenerator.addDataElement("%d\\0")
+        instructions = listOf(
+                Label("p_print_int"),
+                Push(ARM11Register.LR),
+                Move(ARM11Register.R1, ARM11Register.R0),
+                Load(ARM11Register.R0, StaticRef("msg_$msg")),
+                Add(ARM11Register.R0, ARM11Register.R0, 4),
+                Branch(Branch.Mode.LINK, "printf"),
+                Move(ARM11Register.R0, 0),
+                Branch(Branch.Mode.LINK, "fflush"),
+                Pop(ARM11Register.PC),
+        )
+        dependencies = listOf()
+    }
 
 
     override fun getInstructions(): List<ARM11Instruction> {
--- src/instruction/waccLibrary/ThrowOverflowError.kt
+++ src/instruction/waccLibrary/ThrowOverflowError.kt
@@ -1,10 +1,25 @@
 package instruction.waccLibrary
 
-import instruction.ARM11Instruction
+import WACCCodeGenerator
+import instruction.*
+import instruction.addrMode2.ImmOffset
+import instruction.addrMode2.StaticRef
+import register.ARM11Register
 
-class ThrowOverflowError(dataTable: Map<String, Int>) : WACCLibraryFunction(dataTable) {
-    private val instructions: List<ARM11Instruction> = TODO()
-    private val dependencies: List<WACCLibraryFunction> = TODO()
+class ThrowOverflowError(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
+    private val instructions: List<ARM11Instruction>
+    private val dependencies: List<WACCLibraryFunction>
+
+
+    init {
+        val msgCode = codeGenerator.addDataElement("OverflowError: the result is too small/large to store in a 4-byte signed-integer.\\\\n\\\\0")
+        instructions = listOf(
+                Label("p_throw_overflow_error"),
+                Load(ARM11Register.R0, StaticRef("msg_$msgCode")),
+                Branch(Branch.Mode.LINK, "p_throw_overflow_error"),
+        )
+        dependencies = listOf(ThrowOverflowError(codeGenerator))
+    }
 
 
     override fun getInstructions(): List<ARM11Instruction> {
