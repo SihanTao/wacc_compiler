8d6d85bcf354cc87f95b1d26ccf831696bf7fdd7
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2022-03-02 18:47:55 +0000

    Refactor ReadInt

--- src/instruction/waccLibrary/ReadInt.kt
+++ src/instruction/waccLibrary/ReadInt.kt
@@ -5,27 +5,20 @@ import instruction.*
 import instruction.addressing_mode.StaticRef
 import register.Register
 
-class ReadInt(codeGenerator: WACCCodeGenerator) : WACCLibraryFunction(codeGenerator) {
-    private val instructions: List<ARM11Instruction>
-    private val dependencies: List<WACCLibraryFunction>
+class ReadInt() : WACCLibraryFunction() {
+    private val dependencies: List<WACCLibraryFunction> = listOf()
 
-    init {
+    override fun getInstructions(codeGenerator: WACCCodeGenerator): List<ARM11Instruction> {
         val msgCode = codeGenerator.addDataElement("%d\\0")
-        instructions = listOf(
+        return listOf(
                 LABEL("p_read_int"),
                 PUSH(Register.LR),
                 MOV(Register.R1, Register.R0),
                 LDR(Register.R0, StaticRef("msg_$msgCode")),
                 ADD(Register.R0, Register.R0, 4),
-                B(B.Mode.LINK, "scanf"),
+                BL("scanf"),
                 POP(Register.PC)
         )
-        dependencies = listOf()
-    }
-
-
-    override fun getInstructions(): List<ARM11Instruction> {
-        return instructions
     }
 
     override fun getDependencies(): List<WACCLibraryFunction> {
