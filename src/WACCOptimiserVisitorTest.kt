73c2352c492931d8ff45c36ff4c9b8f91aca3f85
Adam Jones <ayj20@ic.ac.uk>
2022-03-14 22:39:24 +0000

    added tests for ProgramNode and FuncNode
    also made tests more concise

--- src/WACCOptimiserVisitorTest.kt
+++ src/WACCOptimiserVisitorTest.kt
@@ -12,7 +12,7 @@ import kotlin.test.assertEquals
 
 internal class SampleTest {
 
-    private val optimiser: WACCOptimiserVisitor = WACCOptimiserVisitor();
+    private val optimiser: WACCOptimiserVisitor = WACCOptimiserVisitor()
     
     /***************************************/
     /* Integer Expression operations tests */
@@ -22,56 +22,49 @@ internal class SampleTest {
     fun testSum() {
         val tree = BinopNode(IntNode(3), IntNode(5), Utils.Binop.PLUS)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 8)
+        assertEquals((optimisedTree as IntNode).value, 8)
     }
 
     @Test
     fun testSubtract() {
         val tree = BinopNode(IntNode(10), IntNode(7), Utils.Binop.MINUS)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 3)
+        assertEquals((optimisedTree as IntNode).value, 3)
     }
 
     @Test
     fun testSubtractNegative() {
         val tree = BinopNode(IntNode(7), IntNode(10), Utils.Binop.MINUS)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, -3)
+        assertEquals((optimisedTree as IntNode).value, -3)
     }
 
     @Test
     fun testMultiply() {
         val tree = BinopNode(IntNode(3), IntNode(5), Utils.Binop.MUL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 15)
+        assertEquals((optimisedTree as IntNode).value, 15)
     }
 
     @Test
     fun testDivide() {
         val tree = BinopNode(IntNode(6), IntNode(2), Utils.Binop.DIV)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 3)
+        assertEquals((optimisedTree as IntNode).value, 3)
     }
 
     @Test
     fun testDivideFloor() {
         val tree = BinopNode(IntNode(7), IntNode(2), Utils.Binop.DIV)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 3)
+        assertEquals((optimisedTree as IntNode).value, 3)
     }
 
     @Test
     fun testModulus() {
         val tree = BinopNode(IntNode(7), IntNode(2), Utils.Binop.MOD)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 1)
+        assertEquals((optimisedTree as IntNode).value, 1)
     }
 
     @Test
@@ -81,8 +74,7 @@ internal class SampleTest {
                 , IntNode(5),
                 Utils.Binop.MINUS)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 1)
+        assertEquals((optimisedTree as IntNode).value, 1)
     }
 
     @Test
@@ -91,8 +83,7 @@ internal class SampleTest {
                 BinopNode(IntNode(8), IntNode(4), Utils.Binop.MINUS),
                 Utils.Binop.MUL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 12)
+        assertEquals((optimisedTree as IntNode).value, 12)
     }
 
     @Test
@@ -108,56 +99,49 @@ internal class SampleTest {
                         Utils.Binop.DIV),
                 Utils.Binop.MUL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, -8)
+        assertEquals((optimisedTree as IntNode).value, -8)
     }
 
     @Test
     fun testIntegerGreaterThan() {
         val tree = BinopNode(IntNode(3), IntNode(1), Utils.Binop.GREATER)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testIntegerGreaterThanEqualToGreater() {
         val tree = BinopNode(IntNode(3), IntNode(1), Utils.Binop.GREATER_EQUAL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testIntegerGreaterThanEqualToEqual() {
         val tree = BinopNode(IntNode(3), IntNode(3), Utils.Binop.GREATER_EQUAL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testIntegerLessThan() {
         val tree = BinopNode(IntNode(3), IntNode(1), Utils.Binop.LESS)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, false)
+        assertEquals((optimisedTree as BoolNode).`val`, false)
     }
 
     @Test
     fun testIntegerLessThanEqualToLess() {
         val tree = BinopNode(IntNode(3), IntNode(5), Utils.Binop.LESS_EQUAL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testIntegerLessThanEqualToEqual() {
         val tree = BinopNode(IntNode(3), IntNode(3), Utils.Binop.LESS_EQUAL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
@@ -173,8 +157,7 @@ internal class SampleTest {
                         Utils.Binop.DIV),
                 Utils.Binop.GREATER)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     /***************************************/
@@ -185,48 +168,42 @@ internal class SampleTest {
     fun testCharGreaterThan() {
         val tree = BinopNode(CharNode("'e'"), CharNode("'c'"), Utils.Binop.GREATER)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testCharGreaterThanEqualToGreater() {
         val tree = BinopNode(CharNode("'e'"), CharNode("'a'"), Utils.Binop.GREATER_EQUAL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testCharGreaterThanEqualToEqual() {
         val tree = BinopNode(CharNode("'c'"), CharNode("'c'"), Utils.Binop.GREATER_EQUAL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testCharLessThan() {
         val tree = BinopNode(CharNode("'e'"), CharNode("'c'"), Utils.Binop.LESS)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, false)
+        assertEquals((optimisedTree as BoolNode).`val`, false)
     }
 
     @Test
     fun testCharLessThanEqualToLess() {
         val tree = BinopNode(CharNode("'f'"), CharNode("'j'"), Utils.Binop.LESS_EQUAL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testCharLessThanEqualToEqual() {
         val tree = BinopNode(CharNode("'f'"), CharNode("'f'"), Utils.Binop.LESS_EQUAL)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     /***************************************/
@@ -237,32 +214,28 @@ internal class SampleTest {
     fun testBoolAndTrue() {
         val tree = BinopNode(BoolNode(true), BoolNode(true), Utils.Binop.AND)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testBoolAndFalse() {
         val tree = BinopNode(BoolNode(true), BoolNode(false), Utils.Binop.AND)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, false)
+        assertEquals((optimisedTree as BoolNode).`val`, false)
     }
 
     @Test
     fun testBoolOrTrue() {
         val tree = BinopNode(BoolNode(true), BoolNode(false), Utils.Binop.OR)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testBoolOrFalse() {
         val tree = BinopNode(BoolNode(false), BoolNode(false), Utils.Binop.OR)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, false)
+        assertEquals((optimisedTree as BoolNode).`val`, false)
     }
 
     @Test
@@ -272,8 +245,7 @@ internal class SampleTest {
                 BinopNode(BoolNode(true), BoolNode(true), Utils.Binop.AND),
                 Utils.Binop.OR)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
@@ -289,8 +261,7 @@ internal class SampleTest {
                         Utils.Binop.AND),
                 Utils.Binop.OR)
         val optimisedTree = optimiser.visitBinopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     /***************************************/
@@ -301,48 +272,42 @@ internal class SampleTest {
     fun testNot() {
         val tree = UnopNode(BoolNode(false), Utils.Unop.NOT)
         val optimisedTree = optimiser.visitUnopNode(tree)
-        assertIs<BoolNode>(optimisedTree)
-        assertEquals(optimisedTree.`val`, true)
+        assertEquals((optimisedTree as BoolNode).`val`, true)
     }
 
     @Test
     fun testMinus() {
         val tree = UnopNode(IntNode(5), Utils.Unop.MINUS)
         val optimisedTree = optimiser.visitUnopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, -5)
+        assertEquals((optimisedTree as IntNode).value, -5)
     }
 
     @Test
     fun testOrd() {
         val tree = UnopNode(CharNode("'c'"), Utils.Unop.ORD)
         val optimisedTree = optimiser.visitUnopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 99)
+        assertEquals((optimisedTree as IntNode).value, 99)
     }
 
     @Test
     fun testChr() {
         val tree = UnopNode(IntNode(99), Utils.Unop.CHR)
         val optimisedTree = optimiser.visitUnopNode(tree)
-        assertIs<CharNode>(optimisedTree)
-        assertEquals(optimisedTree.char, 'c')
+        assertEquals((optimisedTree as CharNode).char, 'c')
     }
 
     @Test
     fun testOrdChrInverse() {
         val tree = UnopNode(UnopNode(IntNode(110), Utils.Unop.CHR), Utils.Unop.ORD)
         val optimisedTree = optimiser.visitUnopNode(tree)
-        assertIs<IntNode>(optimisedTree)
-        assertEquals(optimisedTree.value, 110)
+        assertEquals((optimisedTree as IntNode).value, 110)
     }
 
     @Test
     fun testChrOrdInverse() {
         val tree = UnopNode(UnopNode(CharNode("'f'"), Utils.Unop.ORD),Utils.Unop.CHR)
         val optimisedTree = optimiser.visitUnopNode(tree)
-        assertIs<CharNode>(optimisedTree)
-        assertEquals(optimisedTree.char, 'f')
+        assertEquals((optimisedTree as CharNode).char, 'f')
     }
 
     /***************************************/
@@ -408,8 +373,7 @@ internal class SampleTest {
                 BinopNode(IntNode(3), IntNode(5), Utils.Binop.PLUS)))
         optimiser.visitScopeNode(tree)
         val stat = tree.body[0]
-        assertIs<ReturnNode>(stat)
-        assertIs<IntNode>(stat.expr)
+        assertIs<IntNode>((stat as ReturnNode).expr)
     }
 
     @Test
@@ -418,19 +382,48 @@ internal class SampleTest {
                     BinopNode(IntNode(3), IntNode(5), Utils.Binop.PLUS)),
                 ReturnNode(BinopNode(IntNode(3), IntNode(5), Utils.Binop.PLUS)))
         optimiser.visitSequenceNode(tree)
-        val stat1 = tree.body[0]
-        val stat2 = tree.body[1]
-        assertIs<DeclareStatNode>(stat1)
-        assertIs<IntNode>(stat1.rhs)
-        assertIs<ReturnNode>(stat2)
-        assertIs<IntNode>(stat2.expr)
+        assertIs<IntNode>((tree.body[0] as DeclareStatNode).rhs)
+        assertIs<IntNode>((tree.body[1] as ReturnNode).expr)
     }
 
     /***************************************/
     /* Other node tests                    */
     /***************************************/
 
+    @Test
+    fun testFunc() {
+        val tree = FuncNode("x", BasicType(BasicTypeEnum.INTEGER),
+                ReturnNode(BinopNode(IntNode(3), IntNode(5),
+                        Utils.Binop.PLUS)), listOf())
+        optimiser.visitFuncNode(tree)
+        val stat = tree.functionBody
+        assertIs<IntNode>((stat as ReturnNode).expr)
+    }
 
+    @Test
+    fun testProgramFunctions() {
+        val functions = mutableMapOf(
+                "x" to FuncNode("x", BasicType(BasicTypeEnum.INTEGER),
+                ReturnNode(BinopNode(IntNode(3), IntNode(5),
+                        Utils.Binop.PLUS)),
+                listOf())
+        )
+        val body = SkipNode()
+        val tree = ProgramNode(functions, body)
+        optimiser.visitProgramNode(tree)
+        assertIs<IntNode>((tree.functions["x"]?.functionBody
+                as ReturnNode).expr)
+    }
+
+    @Test
+    fun testProgramBody() {
+        val functions = mutableMapOf<String, FuncNode>()
+        val body = ReturnNode(BinopNode(IntNode(3), IntNode(5),
+                Utils.Binop.PLUS))
+        val tree = ProgramNode(functions, body)
+        optimiser.visitProgramNode(tree)
+        assertIs<IntNode>((tree.body as ReturnNode).expr)
+    }
 
     /***************************************/
     /* Variable replacement tests          */
