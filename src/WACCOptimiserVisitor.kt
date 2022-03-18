a2b7fccc049d1e98eee994595bfb92c849606f7e
Adam Jones <ayj20@ic.ac.uk>
2022-03-18 16:35:52 +0000

    comment out constant propagation of identifiers

--- src/WACCOptimiserVisitor.kt
+++ src/WACCOptimiserVisitor.kt
@@ -451,10 +451,10 @@ class WACCOptimiserVisitor(optimisationLevel: Int) {
 	// which it tries to reduce
 	fun visitIdentNode(node: IdentNode): ExprNode? {
 		if (constantPropagationAnalysis) {
-			val newExpr = currSymbolTable!!.lookupAll(node.name)
+			/*val newExpr = currSymbolTable!!.lookupAll(node.name)
 			if (newExpr != null) {
 				return visitExprNode(newExpr) ?: newExpr
-			}
+			}*/
 		}
 		return null
 	}
